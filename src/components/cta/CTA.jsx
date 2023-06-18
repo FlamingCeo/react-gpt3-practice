7dfe71ea23a14c2a672de0da4e8c0964f4eb059b
Sarah <khan.sarah630@gmail.com>
2023-06-18 23:31:01 +0600

    completed cta module - without following the video guide! yay

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import  './cta.css'
-function CTA () {
+import React from "react";
+import "./cta.css";
+function CTA() {
   return (
-    <div>
-      cta
+    <div className="gpt3__cta-background ">
+      <div className="gpt3__cta-title">
+      <div className="gpt3__cta-small-title">
+        Request Early Access to Get Started
+      </div>
+      <div className="gpt3__cta-small-title-2">
+        Register today & start exploring the endless possiblities.
+      </div>
+      </div>
+
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+        </div>
     </div>
-  )
+  );
 }
 
-export default CTA 
+export default CTA;
